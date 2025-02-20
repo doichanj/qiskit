e149c5834adf682776e73047e79d7e5edf20027d
U-AzureAD\JUNDOI <doichan@jp.ibm.com>
2025-02-20 14:55:35 +0900

    test

--- test/python/quantum_info/operators/symplectic/test_sparse_pauli_op.py
+++ test/python/quantum_info/operators/symplectic/test_sparse_pauli_op.py
@@ -110,6 +110,8 @@ class TestSparsePauliOpMethods(QiskitTestCase):
         print("  =========  sum_op =====================", num_qubits,num_ops,param)
         value = sum_op.to_matrix()
         print("  =========  value =====================", num_qubits,num_ops,param)
+        for op in ops:
+            print(op.to_matrix())
 #        target_operator = sum((op.to_matrix() for op in ops[1:]), ops[0].to_matrix())
 #        print("  =========  target_operator =====================", num_qubits,num_ops,param)
 #        if param is not None:
